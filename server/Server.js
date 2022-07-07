ae1e047ed2516dd6e52f70a524c5fc2a93a82265
Joshua759611 <musyokijoxhua@gmail.com>
2022-07-07 13:22:36 -0400

    The progress is good

--- server/Server.js
+++ server/Server.js
@@ -3,10 +3,29 @@ const cookieSession=require("cookie-session");
 const cors=require("cors");
 const app =express();
 
+
 var corsOptions= {
     origin:"http://localhost:8081"
 };
 app.use(cors(corsOptions));
+// opening mongoose connection to MongoDB
+const db=require("./app/models");
+const { connect } = require("mongoose");
+const { count } = require("../app/models/role.model");
+const Role=db.role;
+db.mongoose;
+connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+.then(()=>{
+    console.log("Successfully Connected to MongoDB.");
+    initial();
+})
+.catch(err=>{
+    console.error("Connection Error", err);
+    process.exit();
+});
 
 //parse requests for content-type -application/json
 app.use(express.json());
@@ -30,3 +49,35 @@ const PORT =process.env.PORT || 8080;
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}.`);
 });
+function initial(){
+    Role.estimatedDocumentCount((err,count)=>{
+        if(!err&&count===0){
+            new Role({
+                name:"user"
+            }).save(err=>{
+                if(err){
+                    console.log("error",err);
+                }
+                console.log("Added 'user' to roles collection");
+
+            });
+            
+        new Role({
+                name:"moderator"
+            }).save(err=>{
+                if(err){
+                    console.log("error",err);
+                }
+                console.log("added 'moderator' to roles connection");
+            });
+            new Role({
+                name:"admin"
+            }).save(err=>{
+                if(err){
+                    console.log("error",err);
+                }
+                console.log("added 'admin' to roles connection");
+            });
+        }
+    })
+}
